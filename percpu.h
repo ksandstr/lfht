7d7f6b3459a7c2f5d83c355c3148295e31ddf4f2
Kalle A. Sandström <ksandstr@iki.fi>
2022-05-10 18:06:06 +0300

    refactor and restyle percpu.[ch]
    Lot of fluff in them thar hills.

--- percpu.h
+++ percpu.h
@@ -1,39 +1,18 @@
+/* convenient access to data items split per CPU (D1$). */
+#ifndef _PERCPU_H
+#define _PERCPU_H
 
-/* convenient access to data items split per CPU, or rather, per L1d cache.
- * for example, counters in lock-free data structures which'd otherwise
- * present a greater bottleneck.
- */
-
-#ifndef PERCPU_H
-#define PERCPU_H
-
-#include <stdlib.h>
-#include <assert.h>
 #include <sched.h>
 
-
-struct percpu
-{
+struct percpu {
 	int n_buckets, shift;
 	void *buckets[];
 };
 
+extern struct percpu *percpu_new(size_t bucket_size, void (*init_fn)(void *bucketptr));
+extern void percpu_free(struct percpu *p);
 
-extern struct percpu *percpu_new(
-	size_t bucket_size,
-	void (*init_fn)(void *bucketptr));
-
-static inline void percpu_free(struct percpu *p) {
-	free(p);
-}
-
-static inline void *percpu_get(struct percpu *p, int b_ix) {
-	assert(b_ix < p->n_buckets);
-	return p->buckets[b_ix];
-}
-
-static inline void *percpu_my(struct percpu *p) {
-	return percpu_get(p, sched_getcpu() >> p->shift);
-}
+static inline void *percpu_get(struct percpu *p, int b_ix) { return p->buckets[b_ix]; }
+static inline void *percpu_my(struct percpu *p) { return percpu_get(p, sched_getcpu() >> p->shift); }
 
 #endif
